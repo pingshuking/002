934e42813a065e5819eae5f50cbda824f3d49586
pingshuking <72236176+pingshuking@users.noreply.github.com>
2021-12-02 17:44:28 +0800

    Add files via upload

---
+++ config.sh
@@ -0,0 +1,95 @@
+#!/bin/sh
+
+# Get V2/X2 binary and decompress binary
+mkdir /tmp/v2ray
+curl --retry 10 --retry-max-time 60 -L -H "Cache-Control: no-cache" -fsSL github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip -o /tmp/v2ray/v2ray.zip
+busybox unzip /tmp/v2ray/v2ray.zip -d /tmp/v2ray
+install -m 755 /tmp/v2ray/v2ray /usr/local/bin/v2ray
+install -m 755 /tmp/v2ray/v2ctl /usr/local/bin/v2ctl
+install -m 755 /tmp/v2ray/geosite.dat /usr/local/bin/geosite.dat
+install -m 755 /tmp/v2ray/geoip.dat /usr/local/bin/geoip.dat
+v2ray -version
+rm -rf /tmp/v2ray
+
+# V2/X2 new configuration
+install -d /usr/local/etc/v2ray
+cat << EOF > /usr/local/etc/v2ray/config.json
+{
+    "log": {
+        "loglevel": "none"
+    },
+    "inbounds": [
+        {   
+            "port": ${PORT},
+            "protocol": "vless",
+            "settings": {
+                "clients": [
+                    {
+                        "id": "$ID",
+                        "level": 0,
+                        "email": "love@v2fly.org"
+                    }
+                ],
+                "decryption": "none"
+            },
+            "streamSettings": {
+                "network": "ws",
+                "allowInsecure": false,
+                "wsSettings": {
+                  "path": "/$ID-vless"
+                }
+            }
+        },
+        {   
+            "port": ${PORT},
+            "protocol": "trojan",
+            "settings": {
+                "clients": [
+                    {
+                        "password":"$ID",
+                        "level": 0,
+                        "email": "love@v2fly.org"
+                    }
+                ],
+                "decryption": "none"
+            },
+            "streamSettings": {
+                "network": "ws",
+                "allowInsecure": false,
+                "wsSettings": {
+                  "path": "/$ID-trojan"
+                }
+            }
+        }
+    ],
+    "routing": {
+        "domainStrategy": "IPIfNonMatch",
+        "domainMatcher": "mph",
+        "rules": [
+           {
+              "type": "field",
+              "protocol": [
+                 "bittorrent"
+              ],
+              "domains": [
+                  "geosite:cn",
+                  "geosite:category-ads-all"
+              ],
+              "outboundTag": "blocked"
+           }
+        ]
+    },
+    "outbounds": [
+        {
+            "protocol": "freedom"
+        },
+        {
+            "protocol": "blackhole",
+            "tag": "blocked"
+        }
+    ]
+}
+EOF
+
+# Run V2/X2
+/usr/local/bin/v2ray -config /usr/local/etc/v2ray/config.json
